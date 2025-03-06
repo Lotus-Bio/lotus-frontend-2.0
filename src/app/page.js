d4a327e9aed2fac4cce0ab7065ada06cd0f3f775
Cauê Netto <caue.netto234@gmail.com>
2025-03-06 12:44:46 -0300

    remove imports desnecessários

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,5 @@
 "use client";
 import { useState } from "react";
-import { Leaf, Heart, PhoneCall } from "lucide-react";
-import Button from "@/ui/components/Button";
-import SubtitleSection from "@/ui/components/SubtitleSection";
-import TitleSection from "@/ui/components/TitleSection";
-import CardMission from "@/ui/components/CardMission";
-import CardFeature from "@/ui/components/CardFeature";
-import CardPlan from "@/ui/components/CardPlan";
-import Input from "@/ui/components/Input";
-import ItemContact from "@/ui/components/ItemContact";
-import Logo from "@/ui/components/Logo";
 import HeaderSection from "@/ui/sections/HeaderSection";
 import MissionSection from "@/ui/sections/MissionSection";
 import FeatureSection from "@/ui/sections/FeatureSection";
