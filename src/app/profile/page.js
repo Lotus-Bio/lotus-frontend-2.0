2a2344ce79f292daf0e177c1b398f75765043c3a
Cauê Netto <caue.netto234@gmail.com>
2025-03-11 23:59:55 -0300

    adiciona conteúdo base da página `profile`

---
+++ src/app/profile/page.js
@@ -0,0 +1,10 @@
+"use client";
+import { Container } from "./profilePage.style";
+
+export default function Profile() {
+  return (
+    <Container>
+      <h1>Profile</h1>
+    </Container>
+  );
+}
