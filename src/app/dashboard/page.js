7cf47eb93533cd86580e63cac2533ca8b9cce33e
Cauê Netto <caue.netto234@gmail.com>
2025-03-11 23:59:32 -0300

    adiciona conteúdo base da página `dashboard`

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,10 @@
+"use client";
+import { Container } from "./dashboardPage.style";
+
+export default function Dashboard() {
+  return (
+    <Container>
+      <h1>dashboard</h1>
+    </Container>
+  );
+}
