48e5b285741d597c69ce764b378181731ac457be
Cauê Netto <caue.netto234@gmail.com>
2025-03-11 23:59:20 -0300

    adiciona conteúdo base da página `calendar`

---
+++ src/app/calendar/page.js
@@ -0,0 +1,10 @@
+"use client";
+import { Container } from "./calendarPage.style";
+
+export default function Calendar() {
+  return (
+    <Container>
+      <h1>Calendar</h1>
+    </Container>
+  );
+}
