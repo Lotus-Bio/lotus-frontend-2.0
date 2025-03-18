39e1ce53bc05c145ab71b8d5387e77cecbccd343
Cauê Netto <caue.netto234@gmail.com>
2025-03-18 16:16:00 -0300

    exibe informações do usuário na dashboard

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,10 +1,21 @@
 "use client";
 import { Container } from "./dashboardPage.style";
+import { useAuth } from "@/context/AuthContext";
 
 export default function Dashboard() {
+  const { user } = useAuth();
+
+  console.log(user);
   return (
     <Container>
       <h1>dashboard</h1>
+
+      {user && (
+        <>
+          <p>Nome: {user.displayName}</p>
+          <p>Email: {user.email}</p>
+        </>
+      )}
     </Container>
   );
 }
