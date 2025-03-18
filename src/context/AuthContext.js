fdce907c0089f7e62cd12ee4aed8d58839a3f22a
Cauê Netto <caue.netto234@gmail.com>
2025-03-18 16:14:42 -0300

    adiciona provider para informações vindas do usuário do firebase

---
+++ src/context/AuthContext.js
@@ -0,0 +1,27 @@
+"use client";
+
+import { createContext, useState, useEffect, useContext } from "react";
+import { auth } from "@/lib/firebase";
+import { onAuthStateChanged } from "firebase/auth";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  const value = { user, loading };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
+
+export const useAuth = () => useContext(AuthContext);
