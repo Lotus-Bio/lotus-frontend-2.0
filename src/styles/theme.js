517f692d2bc112ac41a4948c9582586deac79963
Cauê Netto <caue.netto234@gmail.com>
2025-03-19 20:45:47 -0300

    cria arquivo `theme` para padronizar cores

---
+++ src/styles/theme.js
@@ -0,0 +1,18 @@
+"use client";
+
+const theme = {
+  colors: {
+    white: "rgb(255, 255, 255)",
+    grey: "rgb(75, 85, 99)",
+    lightGrey: "rgb(209, 213, 219)",
+    darkGrey: "rgb(55, 65, 81)",
+    lightGreen: "rgb(92, 156, 84)",
+    darkGreen: "rgb(44, 92, 36)",
+  },
+  fonts: {
+    body: "'Inter', sans-serif",
+    heading: "'Poppins', sans-serif",
+  },
+};
+
+export default theme;
