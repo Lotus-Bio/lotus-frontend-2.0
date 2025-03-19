d57aea1e22c1d5e25e8862ec1f02ce9cee6412ad
Cauê Netto <caue.netto234@gmail.com>
2025-03-19 14:39:09 -0300

    adiciona conteúdo na página de calendário

--- src/app/calendar/calendarPage.style.js
+++ src/app/calendar/calendarPage.style.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  border: 1px solid red;
+  padding: 2rem;
   width: 100%;
   height: 100%;
 `;
--- src/app/calendar/page.js
+++ src/app/calendar/page.js
@@ -1,10 +1,27 @@
 "use client";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import ptLocale from "@fullcalendar/core/locales/pt-br";
+
 import { Container } from "./calendarPage.style";
+import ModalBlur from "@/ui/components/ModalBlur";
 
 export default function Calendar() {
   return (
-    <Container>
-      <h1>Calendar</h1>
-    </Container>
+    <>
+      <ModalBlur />
+      <Container>
+        <FullCalendar
+          plugins={[dayGridPlugin]}
+          initialView="dayGridMonth"
+          events={[
+            { title: "Instalação do equipamento", date: "2025-03-03" },
+            { title: "Início da captação", date: "2025-03-15" },
+            { title: "Fim da captação", date: "2025-03-30" },
+          ]}
+          locale={ptLocale}
+        />
+      </Container>
+    </>
   );
 }
