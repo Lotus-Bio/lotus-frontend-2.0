67ba60dc765416203c42855e8c57b3a0bfcc9339
Cauê Netto <caue.netto234@gmail.com>
2025-03-19 19:13:19 -0300

    altera prop `checked` para `defaultChecked`

--- src/ui/components/NoteItem/index.jsx
+++ src/ui/components/NoteItem/index.jsx
@@ -3,7 +3,7 @@ import { StyledNoteItem } from "./noteItem.style";
 const NoteItem = ({ label }) => {
   return (
     <StyledNoteItem>
-      <input type="checkbox" name="" id="" checked />
+      <input type="checkbox" name="" id="" defaultChecked />
       {label}
     </StyledNoteItem>
   );
