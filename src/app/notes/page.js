57603b7afcbd859257acfdc44052d624e9479b85
Cauê Netto <caue.netto234@gmail.com>
2025-03-19 15:03:25 -0300

    adiciona conteúdo na página de notas

--- src/app/notes/page.js
+++ src/app/notes/page.js
@@ -1,10 +1,38 @@
 "use client";
-import { Container } from "./notesPage.style";
+import NoteItem from "@/ui/components/NoteItem";
+import {
+  Container,
+  Header,
+  Title,
+  Description,
+  Tag,
+  BlockNotes,
+} from "./notesPage.style";
+import ModalBlur from "@/ui/components/ModalBlur";
 
 export default function Notes() {
   return (
-    <Container>
-      <h1>Notes</h1>
-    </Container>
+    <>
+      <ModalBlur />
+      <Container>
+        <Header>
+          <div>
+            <Title>Notas</Title>
+            <Description>
+              Passo a passo das tarefas a serem executadas
+            </Description>
+          </div>
+
+          <Tag>Sistema ativo</Tag>
+        </Header>
+
+        <BlockNotes>
+          <NoteItem label="Instalação do biodigestor" />
+          <NoteItem label="Adicionar matéria orgânica" />
+          <NoteItem label="Monitorar a produção de biogás" />
+          <NoteItem label="Verificar a temperatura do biodigestor:" />
+        </BlockNotes>
+      </Container>
+    </>
   );
 }
