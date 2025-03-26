108cd3d6b3fa4ef0f204b529c69df0c6bfd931aa
Cauê Netto <caue.netto234@gmail.com>
2025-03-26 11:19:38 -0300

    ajusta estilização do loading

--- src/ui/components/Loading/loading.style.js
+++ src/ui/components/Loading/loading.style.js
@@ -4,10 +4,9 @@ export const StyledLoading = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 100vh;
-  width: 100vw;
+  height: 100%;
+  width: 100%;
   position: absolute;
-  top: 0;
   right: 0;
   display: flex;
   justify-content: center;
