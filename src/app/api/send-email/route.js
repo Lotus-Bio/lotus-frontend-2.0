38db5059030736f66ddadd40d4e28b4e00cad07c
Cauê Netto <caue.netto234@gmail.com>
2025-03-26 11:21:07 -0300

    cria lógica de envio de e-mails

---
+++ src/app/api/send-email/route.js
@@ -0,0 +1,33 @@
+import { EmailTemplate } from "@/ui/templates/emailTemplate";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY);
+
+export async function POST(request) {
+  try {
+    const { name, email, message } = await request.json();
+    const emailContent = EmailTemplate({
+      name,
+      email,
+      message,
+    });
+
+    const { data, error } = await resend.emails.send({
+      from: "delivered@resend.dev",
+      to: "caue.netto123@gmail.com",
+      subject: "Contato Lotus",
+      react: emailContent,
+    });
+
+    if (error) {
+      return Response.json({ error }, { status: 500 });
+    }
+
+    return Response.json(data);
+  } catch (error) {
+    return Response.json(
+      { error: error || "Erro inesperado" },
+      { status: 500 }
+    );
+  }
+}
