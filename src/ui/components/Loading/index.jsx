e8bb3d6287efe3b2a070d92c59e82307a9688f6b
Cauê Netto <caue.netto234@gmail.com>
2025-04-08 19:56:42 -0300

    adiciona lógica para barrar scroll enquanto o loading estiver ativo

--- src/ui/components/Loading/index.jsx
+++ src/ui/components/Loading/index.jsx
@@ -1,6 +1,17 @@
+import { useEffect } from "react";
 import { StyledLoading, Spinner } from "./loading.style";
 
 const Loading = ({ loading }) => {
+  useEffect(() => {
+    if (loading) {
+      document.body.style.overflow = "hidden";
+    }
+
+    return () => {
+      document.body.style.overflow = "";
+    };
+  }, [loading]);
+  
   if (!loading) return null;
 
   return (
