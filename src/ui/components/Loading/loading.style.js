493b694327cbece7a29eec17f48ae7c13438d576
Cauê Netto <caue.netto234@gmail.com>
2025-04-08 19:57:04 -0300

    ajusta posicionamento do loading para sempre preencher 100% da tela visivel

--- src/ui/components/Loading/loading.style.js
+++ src/ui/components/Loading/loading.style.js
@@ -4,16 +4,15 @@ export const StyledLoading = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 100%;
-  width: 100%;
-  position: absolute;
-  right: 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  background: rgba(0, 0, 0, 0.8);
+  position: fixed; 
+  top: 0;
+  left: 0; 
+  width: 100vw;
+  height: 100vh; 
+  background: rgba(0, 0, 0, 0.8); 
+  z-index: 9999; 
 `;
+
 
 export const Spinner = styled.div`
   border: 8px solid #f3f3f3;
