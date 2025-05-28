0d9c85a0773acb3ec42b25c27017af9e6a406a46
nettocaue <caue.netto234@gmail.com>
2025-05-28 17:09:41 -0300

    Cria component UserInfo

---
+++ src/ui/components/UserInfo/index.jsx
@@ -0,0 +1,31 @@
+import { StyledUserInfo } from "./userInfo.style";
+import { Mail } from "lucide-react";
+
+const UserInfo = ({ name, email }) => {
+  function getInitials(string) {
+    const words = string.trim().split(" ");
+    if (words.length >= 2) {
+      return words[0][0].toUpperCase() + words[1][0].toUpperCase();
+    }
+    return words[0][0].toUpperCase();
+  }
+
+  const inicial = getInitials(name);
+
+  return (
+    <StyledUserInfo>
+      <div className="circulo">
+        <p>{inicial}</p>
+      </div>
+      <div>
+        <h3>{name}</h3>
+        <p>
+          <Mail size={14} />
+          {email}
+        </p>
+      </div>
+    </StyledUserInfo>
+  );
+};
+
+export default UserInfo;
---
+++ src/ui/components/UserInfo/userInfo.style.js
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+export const StyledUserInfo = styled.div`
+  background: ${({ theme }) => theme.colors.backgroundDark};
+  color: ${({ theme }) => theme.colors.lightGrey};
+  border-radius: 1rem;
+  padding: 1.5rem;
+  border-radius: 0.75rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+
+  margin-top: 2rem;
+  width: fit-content;
+
+  h3 {
+    font-weight: bold;
+    color: ${({ theme }) => theme.colors.white};
+  }
+
+  p {
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+    margin-top: 0.2rem;
+  }
+
+  div.circulo {
+    background: ${({ theme }) => theme.colors.darkActive};
+    color: ${({ theme }) => theme.colors.white};
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 50px;
+    height: 50px;
+    border-radius: 25px;
+  }
+`;
