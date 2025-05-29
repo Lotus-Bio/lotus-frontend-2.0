fb4e5de49d62e83af8a464e051be9f3330e5c037
nettocaue <caue.netto234@gmail.com>
2025-05-28 23:51:40 -0300

    Comenta código da index component Logo

--- src/ui/components/Logo/index.jsx
+++ src/ui/components/Logo/index.jsx
@@ -1,13 +1,13 @@
-import { Leaf } from "lucide-react";
-import { StyledLogo } from "./logo.style";
+//import { Leaf } from "lucide-react";
+//import { StyledLogo } from "./logo.style";
+//
+//const Logo = ({ large = false, light = false }) => {
+//  return (
+//    <StyledLogo $light={light} $large={large}>
+//      <Leaf />
+//      Lotus
+//    </StyledLogo>
+//  );
+//};
 
-const Logo = ({ large = false, light = false }) => {
-  return (
-    <StyledLogo $light={light} $large={large}>
-      <Leaf />
-      Lotus
-    </StyledLogo>
-  );
-};
-
-export default Logo;
+//export default Logo;
